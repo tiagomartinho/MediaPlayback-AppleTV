ffe2373396933db0a822a34d66a2ae48a870c98c
tiagomartinho <tiago.martinho@xpeppers.com>
2016-01-28 21:39:51 +0100

    simplified PlayerViewController

--- MediaPlayback/PlayerViewController.swift
+++ MediaPlayback/PlayerViewController.swift
@@ -6,11 +6,9 @@ class PlayerViewController: AVPlayerViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         if let mediaURL = NSURL(string: "https://download.blender.org/durian/trailer/sintel_trailer-1080p.mp4") {
-            let asset = AVURLAsset(URL: mediaURL)
-            let playerItem = AVPlayerItem(asset: asset)
-            let player = AVPlayer(playerItem: playerItem)
-            self.player = player
-            player.play()
+            let playerItem = AVPlayerItem(URL: mediaURL)
+            self.player = AVPlayer(playerItem: playerItem)
+            self.player?.play()
         }
     }
 }
