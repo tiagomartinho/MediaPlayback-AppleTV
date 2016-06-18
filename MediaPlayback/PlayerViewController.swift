31750bcf6479179531560c72b41486f77bdc04f9
tiagomartinho <tiago.martinho@xpeppers.com>
2016-06-18 15:13:01 -0700

    adds video gravity fill

--- MediaPlayback/PlayerViewController.swift
+++ MediaPlayback/PlayerViewController.swift
@@ -2,14 +2,13 @@ import UIKit
 import AVKit
 
 class PlayerViewController: AVPlayerViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let mediaURL = NSURL(string: "https://download.blender.org/durian/trailer/sintel_trailer-1080p.mp4") {
+        if let mediaURL = NSURL(string: "http://stream.taksimbilisim.com:1935/kanal3/bant1/Kanal3.m3u8") {
             let playerItem = AVPlayerItem(URL: mediaURL)
             self.player = AVPlayer(playerItem: playerItem)
             self.player?.play()
+            self.videoGravity = AVLayerVideoGravityResizeAspectFill
         }
     }
 }
-
