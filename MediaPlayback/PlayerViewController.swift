2d52c00d23b13aa8696b186e7dff93a5c7490a71
tiagomartinho <tiago.martinho@xpeppers.com>
2016-07-10 12:50:45 +0200

    adds playback status observer

--- MediaPlayback/PlayerViewController.swift
+++ MediaPlayback/PlayerViewController.swift
@@ -2,13 +2,57 @@ import UIKit
 import AVKit
 
 class PlayerViewController: AVPlayerViewController {
+
+    let kStatusKey = "status"
+
+    var playerItem: AVPlayerItem?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let mediaURL = NSURL(string: "http://stream.taksimbilisim.com:1935/kanal3/bant1/Kanal3.m3u8") {
-            let playerItem = AVPlayerItem(URL: mediaURL)
-            self.player = AVPlayer(playerItem: playerItem)
-            self.player?.play()
+
+        if let mediaURL = NSURL(string: "http://cdn.ebound.tv/tv/masalatv/chunks.m3u8") {
+            playerItem = AVPlayerItem(URL: mediaURL)
+            if let playerItem = playerItem {
+                self.player = AVPlayer(playerItem: playerItem)
+                self.player?.play()
+            }
+            playerItem?.addObserver(self, forKeyPath: kStatusKey, options: .New, context: nil)
             self.videoGravity = AVLayerVideoGravityResizeAspectFill
+        }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch { }
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        playerItem?.removeObserver(self, forKeyPath: kStatusKey)
+        playerItem = nil
+    }
+
+    deinit {
+        playerItem?.removeObserver(self, forKeyPath: kStatusKey)
+        playerItem = nil
+    }
+
+    internal override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+
+        if keyPath == kStatusKey, let playerItem = object as? AVPlayerItem {
+
+            print("playerItem.status \(playerItem.status.rawValue)")
+
+            if playerItem.status == .Failed {
+                print("playbackDidFail")
+            }
+
+            if playerItem.status == .ReadyToPlay {
+                print("playbackDidSucceed")
+            }
+
+            UIApplication.sharedApplication().idleTimerDisabled = playerItem.status == .ReadyToPlay
         }
     }
 }
